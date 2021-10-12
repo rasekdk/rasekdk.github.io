c388854b8e7653c83ce78d199f5f3652a08e4dd1
César Nóvoa <themotokar@gmail.com>
2021-10-12 21:33:48 +0200

    Created Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import { Route, Switch } from "react-router";
 
 import Header from "./components/elements/Header";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Home from "./components/pages/Home";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -8,6 +12,17 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
     </div>
   );
 }
