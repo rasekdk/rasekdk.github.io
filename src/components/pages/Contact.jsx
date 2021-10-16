e5d6fa72f21da6514953bc6a6295010262bfb65d
César Nóvoa <themotokar@gmail.com>
2021-10-16 16:31:48 +0200

    Contact page Updated

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 
+import Logo from "../atoms/Logo";
+
 function Contact() {
   return (
     <main>
-      <h1>Contact</h1>
+      <section>
+        <h1>
+          <Logo type="contact" />
+        </h1>
+        <h2 className="subtitle_low">
+          <p>Feel free to contact with me if you want to know more about me.</p>
+        </h2>
+        <div className="job_description">
+          <h3>.email()</h3>
+          <a href="mailto:themotokar@gmail.com">&#62; themotokar@gmail.com</a>
+        </div>
+        <div className="job_description">
+          <h3>.phone()</h3>
+          <a href="tel:+34698139758">&#62; +34 698 139 758</a>
+        </div>
+      </section>
     </main>
   );
 }
