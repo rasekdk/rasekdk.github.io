c1905eb7f9df30510ec8f9a4a4760fbca2640e2b
César Nóvoa <themotokar@gmail.com>
2021-10-17 21:00:17 +0200

    HomeEnd Updated

--- src/components/pages/Home/Sections/HomeEnd.jsx
+++ src/components/pages/Home/Sections/HomeEnd.jsx
@@ -7,28 +7,7 @@ function HomeEnd() {
     <section>
       <h3>Thank for checking my website</h3>
       <span className="subtitle_low">&#60;/&#62;</span>
-      {/* <div className="job_description">
-        <h3>.about()</h3>
-        <p>It was hard but also incredible experience.</p>
-        <p>
-          A fullstack web development bootcamp based on{" "}
-          <strong className="high_color">React.js</strong>,{" "}
-          <strong className="high_color">Node.js</strong> and{" "}
-          <strong className="high_color">MySQL</strong>.
-        </p>
-      </div> */}
-      <div className="job_skills">
-        <h3>.learned()</h3>
-        <ul className="job_skills__list">
-          <JobSkill>AdobeSuite</JobSkill>
-          <JobSkill>HTML</JobSkill>
-          <JobSkill>CSS</JobSkill>
-          <JobSkill>3dModel</JobSkill>
-          <JobSkill>VideoEdition</JobSkill>
-          <JobSkill>UX</JobSkill>
-          <JobSkill>UI</JobSkill>
-        </ul>
-      </div>
+      <p>It's a work in progress web site so check it back often!</p>
     </section>
   );
 }
