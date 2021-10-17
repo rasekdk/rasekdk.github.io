8b3089afe2fe7ce1cdc65241763d82882fb9469b
César Nóvoa <themotokar@gmail.com>
2021-10-17 22:31:02 +0200

    Added PageCharger Component

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 import Logo from "../atoms/Logo";
+import PageCharger from "../PageCharger";
 
 function Contact() {
   return (
-    <main>
+    <PageCharger>
       <section>
         <h1>
           <Logo type="contact" />
@@ -12,16 +13,16 @@ function Contact() {
         <h2 className="subtitle_low">
           <p>Feel free to contact with me if you want to know more about me.</p>
         </h2>
-        <div className="job_description">
+        <div className="section_description">
           <h3>.email()</h3>
           <a href="mailto:themotokar@gmail.com">&#62; themotokar@gmail.com</a>
         </div>
-        <div className="job_description">
+        <div className="section_description">
           <h3>.phone()</h3>
           <a href="tel:+34698139758">&#62; +34 698 139 758</a>
         </div>
       </section>
-    </main>
+    </PageCharger>
   );
 }
 
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -8,55 +8,41 @@ import StudyHabProject from "./Sections/StudyHabProject";
 import StudyDesign from "./Sections/StudyDesign";
 import HomeEnd from "./Sections/HomeEnd";
 
-import { motion } from "framer-motion";
-import Charger from "../../atoms/Charger";
+import PageCharger from "../../PageCharger";
 
 function Home() {
   return (
-    <>
-      <Charger />
-      <motion.main
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ delay: 0.75, duration: 0.5, delayChildren: 0.25 }}
-      >
-        <motion.section
-          initial={{ x: 100 }}
-          animate={{ x: 0 }}
-          exit={{ x: 100 }}
-          transition={{ duration: 0.25 }}
-        >
-          <h1>
-            <Logo type={"logo"} />
-          </h1>
-          <h2 className="subtitle">César Nóvoa</h2>
-          <h3 className="subtitle_low">Web Developer & Graphic Designer</h3>
-        </motion.section>
-        <section>
-          <h2>
-            Curriculum
-            <br className="show_md" />
-            .show(<span className="high_color">'César'</span>)
-          </h2>
-          <span className="subtitle_low">&#60;It's a low way.../&#62;</span>
-        </section>
-        {/* ingenyus* web developer */}
-        <WebIngenyus />
-        {/* Project Biojuego */}
-        <WebBiojuego />
-        {/* Studies Hack a Boss */}
-        <StudyHab />
-        {/* Project Hack a Boss */}
-        <StudyHabProject />
-        {/* ingenyus* graphic designer */}
-        <DesignIngenyus />
-        {/* Studies MM */}
-        <StudyDesign />
-        {/* Home End */}
-        <HomeEnd />
-      </motion.main>
-    </>
+    <PageCharger>
+      <section>
+        <h1>
+          <Logo type={"logo"} />
+        </h1>
+        <h2 className="subtitle">César Nóvoa</h2>
+        <h3 className="subtitle_low">Web Developer & Graphic Designer</h3>
+      </section>
+      <section>
+        <h2>
+          Curriculum
+          <br className="show_md" />
+          .show(<span className="high_color">'César'</span>)
+        </h2>
+        <span className="subtitle_low">&#60;It's a low way.../&#62;</span>
+      </section>
+      {/* ingenyus* web developer */}
+      <WebIngenyus />
+      {/* Project Biojuego */}
+      <WebBiojuego />
+      {/* Studies Hack a Boss */}
+      <StudyHab />
+      {/* Project Hack a Boss */}
+      <StudyHabProject />
+      {/* ingenyus* graphic designer */}
+      <DesignIngenyus />
+      {/* Studies MM */}
+      <StudyDesign />
+      {/* Home End */}
+      <HomeEnd />
+    </PageCharger>
   );
 }
 
