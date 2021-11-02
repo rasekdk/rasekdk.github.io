ad2120f2860037d3b92b7dd2904ad294d1cd3af9
César Nóvoa <themotokar@gmail.com>
2021-11-02 20:39:26 +0100

    Moved to a new component

---
+++ src/components/elements/Nav.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Logo from "../atoms/Logo";
+import { NavLink } from "react-router-dom";
+
+function Nav({ onClick, className }) {
+  return (
+    <nav className={className}>
+      <NavLink exact to="/" onClick={onClick}>
+        <Logo type={"logo"} />
+      </NavLink>
+      <NavLink exact to="/about" onClick={onClick}>
+        <Logo type={"about"} />
+      </NavLink>
+      <NavLink exact to="/projects" onClick={onClick}>
+        <Logo type={"projects"} />
+      </NavLink>
+      <NavLink exact to="/contact" onClick={onClick}>
+        <Logo type={"contact"} />
+      </NavLink>
+    </nav>
+  );
+}
+
+export default Nav;
