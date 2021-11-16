63af5f8dd2ea2943b55b58f65d04c7b4ba8b9a6b
César Nóvoa <themotokar@gmail.com>
2021-11-16 22:16:20 +0100

    Added projects

--- src/components/pages/Projects/Projects.jsx
+++ src/components/pages/Projects/Projects.jsx
@@ -3,6 +3,8 @@ import { Helmet } from "react-helmet";
 
 import Title from "../../atoms/Logo/Title";
 import PageCharger from "../../PageCharger";
+import Biojuego from "./Sections/Biojuego";
+import Community from "./Sections/Community";
 
 function Projects() {
   return (
@@ -19,6 +21,8 @@ function Projects() {
             Here you can find my profesional and personal projects
           </div>
         </section>
+        <Community />
+        <Biojuego />
       </PageCharger>
     </>
   );
